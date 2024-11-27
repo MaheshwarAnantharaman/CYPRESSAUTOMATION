70a8e48fcc99c5c2bac6c500cc16e6ab7a938365
MaheshwarAnantharaman <maheshwaranand06@gmail.com>
2024-11-27 14:08:22 +0530

    Code Changes Done

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,25 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('setNumberInputValue', (selector, value) => { 
+    
+    cy.get(selector).clear().type(value.toString()).should('have.value', value.toString()) 
+
+})
+Cypress.Commands.add('setSliderValue', (sliderSelector, value) => { 
+    
+    cy.get(sliderSelector) .invoke('val', value) .trigger('input', { force: true }) .trigger('change', { force: true }) .should('have.value', value.toString())
+})
+Cypress.Commands.add('verifySliderValue', (sliderSelector, value) => { 
+    
+    cy.get(sliderSelector).should('have.value', value.toString()) 
+
+})
+Cypress.Commands.add('setAndVerifySlider', (numberInputSelector, sliderSelector, value) => { 
+    
+    cy.setNumberInputValue(numberInputSelector, value) 
+    cy.setSliderValue(sliderSelector, value) 
+    cy.verifySliderValue(sliderSelector,value)
+
+})
